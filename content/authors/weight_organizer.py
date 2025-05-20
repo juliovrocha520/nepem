274b9d09afa895fcc1f4de6fc6f2afd78a869e5a
Matheus L Machado <matheuslmachado03@gmail.com>
2025-05-19 22:29:40 -0300

    refatora função update_weights para listar diretórios e melhorar mensagens de log

--- content/authors/weight_organizer.py
+++ content/authors/weight_organizer.py
@@ -6,18 +6,18 @@ authors_dir = os.path.join(os.path.dirname(__file__))
 
 
 def update_weights():
-    # Lista os arquivos
-    authors = [f for f in os.listdir(authors_dir) if f.endswith('_index.md')]
+    # Lista os diretórios
+    authors = [d for d in os.listdir(authors_dir) if os.path.isdir(os.path.join(authors_dir, d))]
 
-    # ignora admin
-    authors = [f for f in authors if f != "admin"]
+    # Ignora o diretório "admin"
+    authors = [d for d in authors if d != "admin"]
 
-    # Ordena os arquivos alfabeticamente
+    # Ordena os diretórios alfabeticamente
     authors.sort()
 
     # Atualiza os pesos nos arquivos _index.md
-    for weight, author_file in enumerate(authors, start=1):
-        index_file = os.path.join(authors_dir, author_file)
+    for weight, author_dir in enumerate(authors, start=1):
+        index_file = os.path.join(authors_dir, author_dir, '_index.md')
 
         if os.path.exists(index_file):
             with open(index_file, 'r', encoding='utf-8') as file:
@@ -29,11 +29,11 @@ def update_weights():
                 with open(index_file, 'w', encoding='utf-8') as file:
                     yaml.dump(content, file, default_flow_style=False, allow_unicode=True)
 
-                print(f"Atualizado: {author_file} -> peso: {weight}")
+                print(f"Atualizado: {author_dir}/_index.md -> peso: {weight}")
             else:
                 raise FileNotFoundError(f"Conteúdo não encontrado no arquivo: {index_file}")
         else:
-            raise FileNotFoundError(f"Arquivo não encontrado: {index_file}")
+            print(f"Aviso: Arquivo '_index.md' não encontrado no diretório: {author_dir}")
 
 
 if __name__ == "__main__":
